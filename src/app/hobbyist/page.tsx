a100a79572be233dae3cf2884ffe9f4effa2c3d0
Leah Zhang <LeahZhang1595995@gmail.com>
2024-04-10 01:16:08 -0400

    setting up the blog

--- src/app/hobbyist/page.tsx
+++ src/app/hobbyist/page.tsx
@@ -1,3 +1,18 @@
+import "./Hobbyist.css"
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({subsets: ["latin"], weight:['200', '400', '500','600', '700']});
+
 export default function Page() {
-    return <p>Art & Us Page</p>;
+    return(
+      <div className="Bg">
+        <div className={['Hobbyist', montserrat.className].join(' ')}>
+          <button >Activism</button>
+          <button >Social Impact</button>
+          <button >Computer Science</button>
+          <button >Art</button>
+
+        </div>
+      </div>
+      
+    );
   }
