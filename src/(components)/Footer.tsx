520cd733ddebc74dc38e0a63b89bb40d5e70aabe
Leah Zhang <LeahZhang1595995@gmail.com>
2024-10-31 12:28:01 -0400

    logo

--- src/(components)/Footer.tsx
+++ src/(components)/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer(){
             <div className="w-[50%] bg-black-color flex  flex-wrap justify-end pt-[2.5em] pb-[1.3em] px-[2em] text-[#B7B597] gap-[2em]">
             {
                FooterInfo.map(({name, link}, index)=>(
-                    <a className="text-[#B7B597]/50 font-normal" href={link} key={index}>{name} </a>
+                    <a className="text-[#B7B597]/50 font-normal hover:text-[#B7B597]/100" href={link} key={index}>{name} </a>
                 )) 
             }
         </div>
